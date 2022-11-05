0ed566afa83f71570ac9edaa11b337e78f9a6dd0
unknown <nguyentandata3@gmail.com>
2022-11-05 20:42:29 +0700

    sua bai edit va delete

--- baitap/baitap.js
+++ baitap/baitap.js
@@ -1,4 +1,3 @@
-
 const getdata = () => {
     return new Promise((reslove) => {
         setTimeout( () => {
--- baitap2/js/style.js
+++ baitap2/js/style.js
@@ -1,15 +1,87 @@
-const APIUSER = 'https://jsonplaceholder.typicode.com/users';
-// axios.get(APIUSER);
-async function data() {
-    showdata = await axios.get(APIUSER);
-    showdata = showdata.data;
-    arr = showdata.map((item) => {
-        console.log( `<li id = ${item.id}> ${item.name} </li>`);
-        return `<li id = ${item.id}> ${item.name} </li>`;
-    });
-    return arr;
+
+const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/users';
+
+
+const main = async () => {
+    try {
+        const data = await axios.get(API_ENDPOINT)
+        render(data.data)
+        
+    } catch (error) {
+        console.error(error)
+    }
 }
-const output = data().then(function (a) {
-    console.log(a);
-});
-document.getElementById('root').innerHTML = output;
+
+const render = users => {
+    const tableEl = document.createElement('table')
+    const headerElString = `<tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Action</th>
+             </tr>`
+    tableEl.innerHTML = headerElString
+
+
+    users.forEach(u => {
+        const rowEl = document.createElement('tr')
+
+        const dataString = `
+            <td>${u.id}</td>
+            <td>
+                <span>${u.name}</span>
+                <input class='hidden' name='id' value='${u.name}'/>
+            </td>
+            <td>
+                <span>${u.phone}</span>
+                <input class='hidden' name='id' value='${u.phone}'/>
+            </td>
+        `
+        rowEl.innerHTML = dataString
+
+
+        // handle delete button
+        const actionEl = document.createElement('td')
+        const deleteBtnEl = document.createElement('button')
+        deleteBtnEl.innerText = "Delete"
+        deleteBtnEl.style.color = 'blue'
+
+        deleteBtnEl.onclick = () => rowEl.remove()
+        
+        const editBtnEl = document.createElement('button')
+        editBtnEl.innerText = 'Edit'
+        editBtnEl.style.color = 'red'
+
+        actionEl.appendChild(deleteBtnEl)
+        actionEl.appendChild(editBtnEl)
+
+            editBtnEl.onclick = () => {
+                if ( editBtnEl.innerText == 'Edit') {
+                    const inputEls = rowEl.querySelectorAll('input')
+                    const spanEls = rowEl.querySelectorAll('span')
+                    spanEls.forEach(el => el.classList.toggle('hidden'))
+                    inputEls.forEach(el => el.classList.toggle('hidden'))
+                    editBtnEl.style.color = 'green'
+                    editBtnEl.innerText = 'Save'
+                }
+                else {
+                    const inputEls = rowEl.querySelectorAll('input')
+                    const spanEls = rowEl.querySelectorAll('span')
+                    spanEls.forEach(el => el.classList.toggle('hidden'))
+                    inputEls.forEach(el => el.classList.toggle('hidden'))
+                    editBtnEl.style.color = 'red'
+                    editBtnEl.innerText = 'Edit'
+                }
+        }
+
+
+
+
+        rowEl.appendChild(actionEl)
+        
+        tableEl.appendChild(rowEl)
+    });
+
+    document.querySelector('#root').appendChild(tableEl)
+}
+main()
